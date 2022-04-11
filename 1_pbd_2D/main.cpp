fa49cb99705d2f4e7517ee8ebdadaa818be0f12d
tomcatmew <helltomcat@gmail.com>
2022-04-11 19:31:33 +0900

    fix comment

--- 1_pbd_2D/main.cpp
+++ 1_pbd_2D/main.cpp
@@ -1,6 +1,6 @@
 #include <cstdlib>
 #include <cstdio>
-#if defined(_WIN32) // windows
+#if defined(_WIN32) // windows necessary setup for Delfem2 Library
 #  define NOMINMAX   // to remove min,max macro
 #  include <windows.h>  // this should come before glfw3.h
 #endif
@@ -22,11 +22,11 @@ double distance2(const T *p0, const T *p1) {
 
 /**
  *
- * @param[out] W elastic energy of a 2D spring  <- this CAN be replaced with constraint function C
- * @param[out] dW differentiation of energy w.r.t. positions
- * @param[in] ap positions of the end points of an edge
- * @param[in] Len initial length of the spring
- * @param[in] stiffness stiffness of the spring
+ * @param[out] W - constraint function C
+ * @param[out] dW - differentiation of function W w.r.t. positions
+ * @param[in] ap - positions of the end points of an edge
+ * @param[in] Len - initial length of the spring
+ * @param[in] stiffness - the stiffness of the spring
  */
 
 void W_Wd_Spring(
@@ -41,17 +41,19 @@ void W_Wd_Spring(
   const double u01[ndim] = {
       (ap[1][0] - ap[0][0]) / len,
       (ap[1][1] - ap[0][1]) / len};
-  const double C = (len - Len);  // the length difference
-  const double dC[nnode][ndim] = {  // gradient of C
+  const double C = (len - Len);       // the length difference
+  const double dC[nnode][ndim] = {    // gradient of C
       {-u01[0], -u01[1]},
       {+u01[0], +u01[1]}};
-  // W = 0.5 * stiffness * C * C;  // Hooke's law. energy is square of length
-  // W = C * stiffness
-  W = C;
+
+  W = C;                               // distance constraint function, same with original paper formula 
+  // W = 0.5 * stiffness * C * C;      // we can also use potential energy instead of constraint function 
+  // W = C * stiffness                 // also we can add stiffness on the constraint function
+
   for (unsigned int ino = 0; ino < nnode; ++ino) {
     for (unsigned int idim = 0; idim < ndim; ++idim) {
       //dW[ino][idim] = stiffness * dC[ino][idim] * C;
-      // dW[ino][idim] = dC[ino][idim] * stiffness;
+      //dW[ino][idim] = dC[ino][idim] * stiffness;
       dW[ino][idim] = dC[ino][idim];
     }
   }
@@ -68,11 +70,13 @@ void StepTimePbdSpring2(
     const std::vector<double> &aMassPointInv) {  // simulation
   const unsigned int np = static_cast<unsigned int>(aXY.size() / 2);  // number of points
   const std::vector<double> aXYt = aXY;
+  // equation of motion
   for (unsigned int ip = 0; ip < np; ++ip) {
     if (aMassPointInv[ip] < 1.0e-5) { continue; }
     aXY[ip * 2 + 0] += dt * aUV[ip * 2 + 0] + dt * dt * gravity[0];
     aXY[ip * 2 + 1] += dt * aUV[ip * 2 + 1] + dt * dt * gravity[1];
-  }// udpate position by gravitation 
+  }
+  // update position by gravity (one step of explicit Euler)
   for (unsigned int il = 0; il < aLine.size() / 2; ++il) {
     unsigned int ip0 = aLine[il * 2 + 0];
     unsigned int ip1 = aLine[il * 2 + 1];
@@ -81,15 +85,18 @@ void StepTimePbdSpring2(
         {aXY[ip0 * 2 + 0], aXY[ip0 * 2 + 1]},
         {aXY[ip1 * 2 + 0], aXY[ip1 * 2 + 1]}};
     double w, dw[2][2];
+
     W_Wd_Spring(
         w, dw,
         ap, Len, stiffness);
+
     double mpi0 = aMassPointInv[ip0];
     double mpi1 = aMassPointInv[ip1];
     double deno = 0.0;
     deno += mpi0 * (dw[0][0] * dw[0][0] + dw[0][1] * dw[0][1]);
     deno += mpi1 * (dw[1][0] * dw[1][0] + dw[1][1] * dw[1][1]);
     if (deno < 1.0e-5) { continue; }
+
     aXY[ip0 * 2 + 0] -= w * dw[0][0] / deno;
     aXY[ip0 * 2 + 1] -= w * dw[0][1] / deno;
     aXY[ip1 * 2 + 0] -= w * dw[1][0] / deno;
@@ -110,11 +117,12 @@ void StepTimePbdSpring2(
   }
 }
 
+
+// using Delfem2 Lib to generate mesh 
 void SettingUpSimulation(
     std::vector<double> &aXY,
     std::vector<unsigned int> &aLine,
     std::vector<double> &aMassPointInv) {
-  // generating mesh inside input polygon (aLoop)
   const std::vector<std::vector<double> > aLoop = {{-1, 0, +1, 0, +1, +0.3, -1, +0.3}};
   std::vector<delfem2::CDynPntSur> aPo2D;
   std::vector<delfem2::CDynTri> aETri;
@@ -126,12 +134,11 @@ void SettingUpSimulation(
   delfem2::CMeshTri2D(
       aXY, aTri,
       aVec2, aETri);
-  // convert triangle elements to line elements
+
   delfem2::MeshLine_MeshElem(
       aLine,
       aTri.data(), aTri.size() / 3,
 	  delfem2::MESHELEM_TRI, aXY.size() / 2);
-  // setting up boundry condition (fixing the both ends of the rectangle)
   aMassPointInv.resize(aXY.size() / 2, 1.0);
   for (unsigned int ip = 0; ip < aXY.size() / 2; ++ip) {
     if (aXY[ip * 2 + 0] < -0.99 || aXY[ip * 2 + 0] > +0.99) {
@@ -168,8 +175,8 @@ int main() {
   const std::vector<double> aXY0 = aXY;  // initial position
   std::vector<double> aUV(aXY.size(), 0.0);
   const double gravity[2] = {0., -10.0};
-  const double stiffness = 1.0e+30;
-  //const double stiffness = 1.0e+4;
+  const double stiffness = 1.0e+5;
+
   double dt = 1.0 / 60.0;
   //
   delfem2::glfw::CViewer2 viewer;
@@ -189,12 +196,12 @@ int main() {
     StepTimePbdSpring2(
         aXY, aUV,
         aXY0, aLine, dt, stiffness, gravity, aMassPointInv);
-    //
+
     viewer.DrawBegin_oldGL();
-    // initial configuration (red)
+    // initial configuration (red color)
     glColor3f(1.f, 0.f, 0.f);
     DrawMeshLine2(aXY0, aLine);
-    // deformed configuration (black)
+    // PBD deformed configuration (black color)
     glColor3f(0.f, 0.f, 0.f);
     DrawMeshLine2(aXY, aLine);
     viewer.SwapBuffers();
